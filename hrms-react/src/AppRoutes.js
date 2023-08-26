c0e8d5228174a053008addb304a7275f91143dd3
PeihanZheng <chittebabu@email.com>
2023-08-26 13:06:51 +0800

    Added mc display page

--- hrms-react/src/AppRoutes.js
+++ hrms-react/src/AppRoutes.js
@@ -1,25 +1,25 @@
 import React, {useContext} from 'react';
-import { Route, Routes } from "react-router-dom"
-import { UserContext } from './UserContext'
-import EmployeeHome from "./pages/EmployeeHome"
-import MCSubmissionDisplay from "./pages/McSubmissionDisplay"
-import Admin from "./pages/Admin"
+import { Route, Routes } from "react-router-dom";
+import { UserContext } from './UserContext';
+import EmployeeHome from "./pages/EmployeeHome";
+import MCSubmissionDisplay from "./pages/McSubmissionDisplay";
+import Admin from "./pages/Admin";
 import UpdateEmployee from './pages/UpdateEmployee';
-import ManagerHome from "./pages/ManagerHome"
-import Dashboard from "./pages/Dashboard"
-import About from "./pages/About"
-import MCsubmission from "./pages/MCsubmissionForm"
-import EmployeeForm from "./pages/EmployeeForm"
-import LogIn from "./pages/LogIn"
-import User from "./pages/User"
-import Policy from "./pages/Policy"
-import ApplyLeaveForm from "./pages/ApplyLeaveForm"
-import ApplyClaimForm from "./pages/ApplyClaimForm"
-import ApplyMcForm from "./pages/ApplyMcForm"
+import ManagerHome from "./pages/ManagerHome";
+import Dashboard from "./pages/Dashboard";
+import About from "./pages/About";
+import MCsubmission from "./pages/MCsubmissionForm";
+import EmployeeForm from "./pages/EmployeeForm";
+import LogIn from "./pages/LogIn";
+import User from "./pages/User";
+import Policy from "./pages/Policy";
+import ApplyLeaveForm from "./pages/ApplyLeaveForm";
+import ApplyClaimForm from "./pages/ApplyClaimForm";
+import ApplyMcForm from "./pages/ApplyMcForm";
 
 
 export default function AppRoutes(){
-    const user = useContext(UserContext)
+    const user = useContext(UserContext);
 
     return(
         <Routes class="position-relative">
--- hrms-react/src/pages/McSubmissionDisplay.js
+++ hrms-react/src/pages/McSubmissionDisplay.js
@@ -1,9 +1,38 @@
 // import modules
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { useNavigate } from 'react-router';
 
 // define component
 const MCSubmissionDisplay = () => {
+    // declare properties
+    const navigate = useNavigate();
+    const [mcSubmissions, setMcSubmissions] = useState([]);
+
+    // declare useEffect hook
+    useEffect(() => {
+        const fetchMcSubmissions = async () => {
+            try {
+                // make GET request to Spring Boot API
+                const response = await axios.get('http://localhost:8080/mc-submissions');
+                console.log(response);
+                setMcSubmissions(response.data);
+            } catch (error) {
+                // error handling
+                console.error(error);
+            }
+        };
+
+        // invoke fetchMcSubmissions
+        fetchMcSubmissions();
+    }, []);
+
+    // redirect back to admin home
+    const handleBack = () => {
+        // redirect to admin home page
+        navigate('/home');
+    }
+
     // return jsx
     return (
         <div className="container h-100 mt-5">
@@ -18,7 +47,27 @@ const MCSubmissionDisplay = () => {
                         <th>Employee Name</th>
                     </tr>
                 </thead>
+                <tbody>
+                    {mcSubmissions.map((mcSubmission) => {
+                        return (
+                            <tr key={mcSubmission.mcId}>
+                                <td>{mcSubmission.mcId}</td>
+                                <td>{mcSubmission.mcDate}</td>
+                                <td>{mcSubmission.reason}</td>
+                                <td>{mcSubmission.fileUpload}</td>
+                                <td>{mcSubmission.employee ? (
+                                    mcSubmission.employee.employeeName
+                                ) : (
+                                    <em>Not Available</em>
+                                )}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
             </table>
+            <div className="d-flex justify-content-between">
+                <button className="btn btn-primary" onClick={handleBack}>Back</button>
+            </div>
         </div>
     );
 };
